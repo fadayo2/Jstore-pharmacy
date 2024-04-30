e27273cce8013828bdf3bcf01f89309f31a623c4
fadayo2 <afadunmiye@gmail.com>
2024-04-30 16:27:14 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let serviceDropdown = document.getElementById('serDrp');
+let marketDrp = document.getElementById('marketDrp');
+
+const hoverOnService = () => {
+    serviceDropdown.style.scale = 1;
+}
+
+const hoverOutService = () => {
+    serviceDropdown.style.scale = 0;
+}
+
+const hoverOnMarket = () => {
+    marketDrp.style.scale = 1;
+}
+
+const hoverOutMarket = () => {
+    marketDrp.style.scale = 0;
+}
